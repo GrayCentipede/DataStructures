078d5a2c2ee0ae1209c5099e039755d86f4535b3
GrayCentipede <maucarrui@gmail.com>
2019-01-11 23:52:47 -0600

    Added skeleton for the unit tests of the doubly linked list

---
+++ python3/test/TestDoublyLinkedList.py
@@ -0,0 +1,73 @@
+import unittest
+from src.DoublyLinkedList import DoublyLinkedList
+
+class TestDoublyLinkedList(unittest.TestCase):
+
+    def test_add(self):
+        pass
+
+    def test_delete(self):
+        pass
+
+    def test_is_empty(self):
+        pass
+
+    def test_get_elements(self):
+        pass
+
+    def test_clear(self):
+        pass
+
+    def test_iterator(self):
+    	pass
+
+    def test_str(self):
+        pass
+
+    def test_repr(self):
+        pass
+
+    def test_eq(self):
+        pass
+
+    def test_equals(self):
+        pass
+
+    def test_len(self):
+        pass
+
+    def test_size(self):
+        pass
+
+    def test_add_last(self):
+        pass
+
+    def test_add_first(self):
+        pass
+
+    def test_remove_last(self):
+        pass
+
+    def test_remove_first(self):
+        pass
+
+    def test_get_last(self):
+        pass
+
+    def test_get_first(self):
+        pass
+
+    def test_get_element(self):
+        pass
+
+    def test_clone(self):
+        pass
+
+    def test_reverse(self):
+        pass
+
+    def test_index_of(self):
+        pass
+
+if __name__ == '__main__':
+	unittest.main()
