2b9cb7b091ee0c4bb90c356c72be986c0efdae71
GrayCentipede <maucarrui@gmail.com>
2019-01-11 23:28:55 -0600

    Added skeleton for doubly linked list

---
+++ python3/src/DoublyLinkedList.py
@@ -0,0 +1,83 @@
+class DoublyLinkedList:
+
+    class Node:
+
+        element  = None
+        next     = None
+        previous = None
+
+        def __init__(self, element):
+            pass
+
+    head   = None
+    tail   = None
+    length = None
+
+    def add(self, element):
+        pass
+
+    def delete(self, element):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def get_elements(self):
+        pass
+
+    def clear(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __next__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __eq__(self):
+        pass
+
+    def equals(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def size(self):
+        pass
+
+    def add_last(self):
+        pass
+
+    def add_first(self):
+        pass
+
+    def remove_last(self):
+        pass
+
+    def remove_first(self):
+        pass
+
+    def get_last(self):
+        pass
+
+    def get_first(self):
+        pass
+
+    def get_element(self, index):
+        pass
+
+    def clone(self):
+        pass
+
+    def reverse(self):
+        pass
+
+    def index_of(self, element):
+        pass
