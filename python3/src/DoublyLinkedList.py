24502d4c9cb3b6e4fc2cce185cb47622738ac8a3
GrayCentipede <maucarrui@gmail.com>
2019-01-17 23:39:17 -0600

    Some methods were added to DoublyLinkedList.py

--- python3/src/DoublyLinkedList.py
+++ python3/src/DoublyLinkedList.py
@@ -7,38 +7,66 @@ class DoublyLinkedList(object):
         previous = None
 
         def __init__(self, element):
-            pass
+            self.element = element
 
     head   = None
     tail   = None
-    length = None
+    length = 0
 
     def add(self, element):
-        pass
+
+        node = self.Node(element)
+
+        if (self.head is None):
+            self.head = self.tail = node
+
+        else:
+        	node.previous = self.tail
+        	self.tail.next = node
+        	self.tail = node
+
+        self.length += 1
 
     def delete(self, element):
         pass
 
     def is_empty(self):
-        pass
+        return (self.length == 0)
 
     def get_elements(self):
-        pass
+        return self.length
 
     def clear(self):
-        pass
+        self.length = 0
+        self.head = self.tail = None
 
     def __iter__(self):
-        pass
+        self.iter_node = self.head
+        return self
 
     def __next__(self):
-        pass
+        if self.iter_node is not None:
+            element = self.iter_node.element
+            self.iter_node = self.iter_node.next
+            return element
+
+        else:
+            raise StopIteration
 
     def __str__(self):
-        pass
+        s = '['
+        counter = 0
+
+        for i in self:
+            if (counter < self.length):
+                s += str(i) + ', '
+            else:
+                s += str(i) + ']'
+
+        return s
 
     def __repr__(self):
-        pass
+        return str(self)
 
     def __eq__(self, object):
         pass
@@ -47,13 +75,13 @@ class DoublyLinkedList(object):
         pass
 
     def __len__(self):
-        pass
+        return self.length
 
     def size(self):
-        pass
+        return self.length
 
     def add_last(self, element):
-        pass
+        self.add(element)
 
     def add_first(self, element):
         pass
@@ -65,13 +93,20 @@ class DoublyLinkedList(object):
         pass
 
     def get_last(self):
-        pass
+        return self.tail.element
 
     def get_first(self):
-        pass
+        return self.head.element
 
     def get_element(self, index):
-        pass
+        counter = 0
+
+        for i in self:
+            if (counter == index):
+                return i
+
+            counter += 1
+
 
     def clone(self):
         pass
@@ -80,7 +115,23 @@ class DoublyLinkedList(object):
         pass
 
     def index_of(self, element):
-        pass
+        counter = 0
+
+        for i in self:
+            if (i == element):
+                return counter
+
+            counter += 1
+
+        raise ValueError(str(element) + 'is not in list')
 
     def contains(self, element):
-        pass
+        counter = 0
+
+        for i in self:
+            if (i == element):
+                return True
+
+            counter += 1
+
+        return False
