9c463a7f29dc43fa48b0ee4c559ad055d1f3e44a
GrayCentipede <maucarrui@gmail.com>
2019-01-20 23:41:21 -0600

    Added stack's skeleton for python 3

---
+++ python3/src/Stack.py
@@ -0,0 +1,32 @@
+class Stack(object):
+
+    class Node(object):
+        element = None
+        next    = None
+
+        def __init__(self, element):
+            pass
+
+    head = None
+    tail = None
+
+    def push(self, element):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def equals(self, obj):
+        pass
+
+    def __eq__(self, obj):
+        pass
+
+    def __str__(self):
+        pass
