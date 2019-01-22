6de1bf38f50a091c5ad66f14d478da3756fa75a5
GrayCentipede <maucarrui@gmail.com>
2019-01-21 18:22:41 -0600

    Added skeleton for the unit tests of the Stacks and Queues in python 3

---
+++ python3/test/TestStack.py
@@ -0,0 +1,28 @@
+import unittest
+import random
+
+from src.Queue import Queue
+from src.Stack import Stack
+
+class TestStack(unittest.TestCase):
+
+    def test_push(self):
+        pass
+
+    def test_pop(self):
+        pass
+
+    def test_peek(self):
+        pass
+
+    def test_is_empty(self):
+        pass
+
+    def test_equals(self):
+        pass
+
+    def test_str(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main(verbosity = 2)
