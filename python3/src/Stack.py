5529a09b278dd04c67526d62c5d10be9402bdfa1
GrayCentipede <maucarrui@gmail.com>
2019-01-22 23:41:01 -0600

    A Stack is not available in python 3

--- python3/src/Stack.py
+++ python3/src/Stack.py
@@ -5,31 +5,102 @@ class Stack(object):
         next    = None
 
         def __init__(self, element):
-            pass
+            self.element = element
 
     head = None
     tail = None
 
     def push(self, element):
-        pass
+        node = self.Node(element)
+
+        if (self.head is None):
+            self.head = node
+
+        else:
+            temp = self.head
+            self.head = node
+            node.next = temp
 
     def pop(self):
-        pass
+        if (self.head is None):
+            raise IndexError('Empty stack')
+
+        e = self.head.element
+
+        if (self.head.next is None):
+            self.head = None
+        else:
+            self.head = self.head.next
+
+        return e
 
     def peek(self):
-        pass
+        if (self.head is None):
+            raise IndexError('Empty stack')
+
+        return self.head.element
 
     def is_empty(self):
-        pass
+        return (self.head is None)
 
     def equals(self, obj):
-        pass
+        if (type(obj) is Stack):
+
+            if (obj.is_empty() and self.is_empty()):
+                return True
+
+            n = self.head
+            m = obj.head
+
+            while ((m is not None) or (n is not None)):
+
+                a = (n is None and m is not None)
+                b = (n is not None and m is None) 
+
+                if (a or b or (n.element != m.element)):
+                    return False
+
+                n = n.next
+                m = m.next
+
+            return True
+
 
     def __eq__(self, obj):
-        pass
+        return self.equals(obj)
 
     def __str__(self):
-        pass
+        s = ""
+
+        n = self.head
+
+        if (n is None):
+            return s
+
+        while (n is not None):
+            if (n.next is None):
+                s += str(n.element)
+            else:
+                s += str(n.element) + '\n'
+
+            n = n.next
+
+        return s
 
     def __repr__(self):
-        pass
+        n = self.head
+
+        if (n is None):
+            return '[]'
+
+        r = "["
+
+        while (n is not None):
+            if (n.next is None):
+                r += str(n.element)
+            else:
+                r += str(n.element) + ', '
+
+            n = n.next
+
+        return r + ']'
