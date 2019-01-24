877461d152f7f3547b8343699921472c42422686
GrayCentipede <maucarrui@gmail.com>
2019-01-23 23:46:28 -0600

    Implemented TestQueue.py unit tests

--- python3/test/TestQueue.py
+++ python3/test/TestQueue.py
@@ -2,27 +2,115 @@ import unittest
 import random
 
 from src.Queue import Queue
-from src.Stack import Stack
 
 class TestQueue(unittest.TestCase):
 
     def test_enqueue(self):
-        pass
+        q = Queue()
+
+        self.assertTrue(q.is_empty())
+        q.enqueue(1)
+        self.assertFalse(q.is_empty())
+
+        self.assertEquals(q.peek(), 1)
+
+        q.enqueue(2)
+        self.assertNotEquals(q.peek(), 2)
 
     def test_dequeue(self):
-        pass
+        q = Queue()
+
+        self.assertRaises(IndexError, q.dequeue)
+
+        for i in range(10):
+            q.enqueue(i)
+
+        for i in range(9):
+            self.assertEquals(q.peek(), i)
+            q.dequeue()
+            self.assertNotEquals(q.peek(), i)
+
+        self.assertFalse(q.is_empty())
+        q.dequeue()
+        self.assertTrue(q.is_empty())
 
     def test_peek(self):
-        pass
+        q = Queue()
+
+        self.assertRaises(IndexError, q.peek)
+
+        q.enqueue(0)
+        self.assertEquals(s.peek(), 0)
+
+        q.enqueue(1)
+        self.assertNotEquals(s.peek(), 1)
+        self.assertEquals(s.peek(), 0)
+
+        q.dequeue()
+        self.assertEquals(s.peek(), 1)        
 
     def test_is_empty(self):
-        pass
+        q = Queue()
+
+        self.assertTrue(q.is_empty())
+
+        for i in range(1000):
+            n = random.randint(0, 1000)
+            q.enqueue(n)
+
+        counter = 0
+        while (not q.is_empty()):
+            q.dequeue()
+            counter += 1
+
+        self.assertEquals(counter, 1000)
 
     def test_equals(self):
-        pass
+        qa = Queue()
+        qb = Queue()
+
+        self.assertEqual(qa, qb)
+
+        for i in range(50):
+            r = random.randint(1, 200)
+
+            qa.enqueue(r)
+            self.assertNotEquals(qa, qb)
+            qb.enqueue(r)
+            self.assertEquals(qa, qb)
+
+        for i in range(50):
+            qa.dequeue()
+            self.assertNotEquals(qa, qb)
+            qb.dequeue()
+            self.assertEquals(qa, qb)
 
     def test_str(self):
-        pass
+        q = Queue()
+
+        self.assertEquals(str(q), '')
+
+        s = ''
+
+        for i in range(50):
+            r = random.random.randint(1, 200)
+
+            q.enqueue(r)
+            if (s == ''):
+                s = str(r)
+            else:
+                s = str(r) + ' ' + s
+                
+
+        q.enqueue(929)
+        s = '929 ' + s
+
+        self.assertEquals(str(q), s)
+
+        s = s[:-4]
+        q.dequeue()
+
+        self.assertEquals(str(q), s)
 
 if __name__ == '__main__':
     unittest.main(verbosity = 2)
