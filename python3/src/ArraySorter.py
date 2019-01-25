145c6912daa87cca722b333309d8b7525eda7f43
GrayCentipede <maucarrui@gmail.com>
2019-01-24 23:22:09 -0600

    Added the skeleton for the array sorter, including unit tests

---
+++ python3/src/ArraySorter.py
@@ -0,0 +1,19 @@
+class ArraySorter(object):
+
+    def bubble_sort(arr):
+        pass
+
+    def selection_sort(arr):
+        pass
+
+    def quick_sort(arr):
+        pass
+
+    def merge_sort(arr):
+        pass
+
+    def insertion_sort(arr):
+        pass
+
+    def bucket_sort(arr):
+        pass
---
+++ python3/test/TestArraySorter.py
@@ -0,0 +1,26 @@
+import unittest
+
+from src.ArraySorter import ArraySorter
+
+class TestArraySorter(unittest.TestCase):
+
+    def test_bubble_sort(self):
+        pass
+
+    def test_selection_sort(self):
+        pass
+
+    def test_quick_sort(self):
+        pass
+
+    def test_merge_sort(self):
+        pass
+
+    def test_insertion_sort(self):
+        pass
+
+    def test_bucket_sort(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main(verbosity = 2)
