2f68cd30e0a479a5d486bbbd0776aa83d61de6f5
Mauricio Carrasco Ruiz <maucarrui@ciencias.unam.mx>
2019-12-22 19:46:11 -0600

    Implemented TestArraySorter.py unit tests.

--- python3/test/TestArraySorter.py
+++ python3/test/TestArraySorter.py
@@ -1,26 +1,107 @@
 import unittest
+import random
 
 from src.ArraySorter import ArraySorter
 
 class TestArraySorter(unittest.TestCase):
 
+    def is_sorted(self, array):
+        n = len(array)
+
+        for i in range(n):
+            if ((i + 1) == n):
+                break
+            if (array[i] > array[i + 1]):
+                return False
+
+        return True
+
+    def generate_random_array(self, n):
+        x = []
+
+        for i in range(n):
+            m = random.randint(0, 360)
+            x.append(m)
+
+        return x
+
     def test_bubble_sort(self):
-        pass
+        a = self.generate_random_array(10)
+        a.append(a[-1] - 1)
+
+        self.assertFalse(self.is_sorted(a))
+
+        sorter = ArraySorter()
+        a = sorter.bubble_sort(a)
+        self.assertTrue(self.is_sorted(a))
+
+        a.append(a[-1] - 1)
+        self.assertFalse(self.is_sorted(a))
 
     def test_selection_sort(self):
-        pass
+        a = self.generate_random_array(10)
+        a.append(a[-1] - 1)
+
+        self.assertFalse(self.is_sorted(a))
+
+        sorter = ArraySorter()
+        a = sorter.selection_sort(a)
+        self.assertTrue(self.is_sorted(a))
+
+        a.append(a[-1] - 1)
+        self.assertFalse(self.is_sorted(a))
 
     def test_quick_sort(self):
-        pass
+        a = self.generate_random_array(10)
+        a.append(a[-1] - 1)
+
+        self.assertFalse(self.is_sorted(a))
+
+        sorter = ArraySorter()
+        a = sorter.test_quick_sort(a)
+        self.assertTrue(self.is_sorted(a))
+
+        a.append(a[-1] - 1)
+        self.assertFalse(self.is_sorted(a))
 
     def test_merge_sort(self):
-        pass
+        a = self.generate_random_array(10)
+        a.append(a[-1] - 1)
+
+        self.assertFalse(self.is_sorted(a))
+
+        sorter = ArraySorter()
+        a = sorter.merge_sort(a)
+        self.assertTrue(self.is_sorted(a))
+
+        a.append(a[-1] - 1)
+        self.assertFalse(self.is_sorted(a))
 
     def test_insertion_sort(self):
-        pass
+        a = self.generate_random_array(10)
+        a.append(a[-1] - 1)
+
+        self.assertFalse(self.is_sorted(a))
+
+        sorter = ArraySorter()
+        a = sorter.insertion_sort(a)
+        self.assertTrue(self.is_sorted(a))
+
+        a.append(a[-1] - 1)
+        self.assertFalse(self.is_sorted(a))
 
     def test_bucket_sort(self):
-        pass
+        a = self.generate_random_array(10)
+        a.append(a[-1] - 1)
+
+        self.assertFalse(self.is_sorted(a))
+
+        sorter = ArraySorter()
+        a = sorter.bucket_sort(a)
+        self.assertTrue(self.is_sorted(a))
+
+        a.append(a[-1] - 1)
+        self.assertFalse(self.is_sorted(a))
 
 if __name__ == '__main__':
     unittest.main(verbosity = 2)
