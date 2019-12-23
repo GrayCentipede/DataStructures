bdc7dea6ab85e3f8fbcbccc8f7c5fee957c50763
Mauricio Carrasco Ruiz <maucarrui@ciencias.unam.mx>
2019-12-22 21:55:15 -0600

    Bubble sort and selection sort are now available in ArraySorter.py

--- python3/src/ArraySorter.py
+++ python3/src/ArraySorter.py
@@ -1,19 +1,48 @@
 class ArraySorter(object):
 
-    def bubble_sort(arr):
+    def swap(self, i, j, array):
+        temp     = array[i]
+        array[i] = array[j]
+        array[j] = temp
+
+    def bubble_sort(self, array):
+        n      = len(array)
+        swaped = True
+
+        while swaped:
+            swaped = False
+
+            for j in range(n - 1):
+                if (array[j] > array[j + 1]):
+                    self.swap(j, j + 1, array)
+                    swaped = True
+
+            if not swaped:
+                break
+
+        return array
+
+
+    def selection_sort(self, array):
+        n = len(array)
+
+        for i in range(n - 1):
+            min_index = i
+            for j in range(i, n):
+                if (array[j] < array[min_index]):
+                    min_index = j
+            self.swap(i, min_index, array)
+
+        return array
+
+    def quick_sort(self, array):
         pass
 
-    def selection_sort(arr):
+    def merge_sort(self, array):
         pass
 
-    def quick_sort(arr):
+    def insertion_sort(self, array):
         pass
 
-    def merge_sort(arr):
-        pass
-
-    def insertion_sort(arr):
-        pass
-
-    def bucket_sort(arr):
+    def bucket_sort(self, array):
         pass
